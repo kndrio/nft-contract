1d58623f5c2321515743e2bd3d6f123caee5d501
Kennedy Carvalho <kennedy.carvalho@mprj.mp.br>
2022-07-30 10:47:24 -0300

    Initial commit - NFT Collection

---
+++ contracts/MyEpicNFT.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+import "hardhat/console.sol";
+
+contract MyEpicNFT {
+    constructor() {
+        console.log("This is my NFT contract. Hooray!");
+    }
+}
